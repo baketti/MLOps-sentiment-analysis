ab1b82a02bae15e235ed4f6f6c31a1623696b0dc
baketti <e.bachetti1997@gmail.com>
2026-03-18 13:43:08 +0100

    feat: Hugging Face model performances evaluation Add two functions to evaluate performances of the following models: - Hugging Face pretrained base model - Hugging Face pretrained base model fine tuned with a custom dataset

---
+++ src/evaluating/evaluate.py
@@ -0,0 +1,51 @@
+from sklearn.metrics import f1_score, accuracy_score
+from transformers import Trainer
+
+
+def evaluate_hf_pretrained_model(pipeline, test_dataset, label2id):
+    """
+        Evaluates the performance of the Hugging Face pretrained model on the test dataset using F1 macro and accuracy metrics.
+        Params:
+            pipeline: The Hugging Face pipeline for text classification.
+            test_dataset: The test dataset as a Hugging Face Dataset.
+            label2id: A dictionary mapping label names to their corresponding IDs.
+        Returns:
+            dict: A dictionary containing the F1 macro score and accuracy of the model on the test dataset.
+    """
+    y_true, y_pred = [], []
+
+    for data in test_dataset:
+        result = pipeline(data["text"])
+        best   = max(result, key=lambda x: x["score"])
+        y_true.append(data["label"])
+        y_pred.append(label2id[best["label"].lower()])
+
+    return {
+        "f1_macro": f1_score(y_true, y_pred, average="macro"),
+        "accuracy": accuracy_score(y_true, y_pred)
+    }
+
+
+def evaluate_hf_fine_tuned_model(trainer: Trainer, quality_thresholds: dict) -> bool:
+    """
+        Evaluates the performance of the Hugging Face fine-tuned model using and checks if it meets the specified quality thresholds for accuracy and F1 score.
+        Params:
+            trainer (Trainer): The Trainer object after fine-tuning the model.
+            quality_thresholds (dict): A dictionary containing the minimum thresholds for accuracy and F1 score to decide whether the model is ready to be pushed to the Hugging Face Hub.
+        Returns:
+            bool: True if the model meets the quality thresholds and is ready to be pushed to the Hugging Face Hub, False otherwise.
+    """
+    is_ready_for_hf_hub = False
+    
+    accuracy_threshold = quality_thresholds.get("accuracy_min", 0.7)
+    f1_score_threshold  = quality_thresholds.get("f1_min", 0.7)
+
+    eval_results = trainer.evaluate()
+
+    accuracy = eval_results.get("eval_accuracy", 0)
+    f1_macro = eval_results.get("eval_f1_macro", 0)
+
+    if (accuracy >= accuracy_threshold) and (f1_macro >= f1_score_threshold):
+        is_ready_for_hf_hub = True
+
+    return is_ready_for_hf_hub
