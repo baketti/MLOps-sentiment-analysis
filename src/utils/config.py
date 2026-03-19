71bcc562860cbd200b4778a59b51ba5f8af5ba86
baketti <e.bachetti1997@gmail.com>
2026-03-19 16:19:41 +0100

    Add docstring

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,13 @@
 import yaml
 
 def load_config(path: str = "config.yaml") -> dict:
+    """
+        Loads the configuration from a YAML file.
+        Params:
+            path (str): The path to the YAML configuration file. Default is "config.yaml".
+        Returns:
+            dict: A dictionary containing the configuration parameters.
+    """
     with open(path, "r") as f:
         return yaml.safe_load(f)
     
