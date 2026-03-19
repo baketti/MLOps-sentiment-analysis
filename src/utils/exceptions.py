f17d1136a7cd7ad0de9cbc509d81e98450787bf9
baketti <e.bachetti1997@gmail.com>
2026-03-19 18:12:20 +0100

    Create custom exceptions for better errors understanding

---
+++ src/utils/exceptions.py
@@ -0,0 +1,47 @@
+class InvalidDatasetStructureError(Exception):
+    """
+        Exception raised when the dataset structure is invalid or does not contain the required columns.
+    """
+    pass
+
+class ConfigLoadError(Exception):
+    """
+        Exception raised when there is an error loading the configuration.
+    """
+    pass
+
+class LoadingDatasetError(Exception):
+    """
+        Exception raised when there is an error loading the dataset.
+    """
+    pass
+
+class FineTuningError(Exception):
+    """
+        Exception raised when there is an error during the fine-tuning process.
+    """
+    pass
+
+class PushingToHubError(Exception):
+    """
+        Exception raised when there is an error pushing the model to the Hugging Face Hub.
+    """
+    pass
+
+class ModelLoadingError(Exception):
+    """
+        Exception raised when there is an error loading a model or tokenizer.
+    """
+    pass
+
+class PredictionError(Exception):
+    """
+        Exception raised when there is an error during sentiment prediction.
+    """
+    pass
+
+class EvaluationError(Exception):
+    """
+        Exception raised when there is an error during model evaluation.
+    """
+    pass
