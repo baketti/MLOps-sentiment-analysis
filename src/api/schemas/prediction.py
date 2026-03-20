e6050cc0eccebf576cbc538b0653f5524658a07a
baketti <e.bachetti1997@gmail.com>
2026-03-21 00:23:27 +0100

    Remove deprecated param

--- src/api/schemas/prediction.py
+++ src/api/schemas/prediction.py
@@ -4,19 +4,15 @@ class PredictRequestBody(BaseModel):
         min_length=1,
         max_length=512,
         description="The input text for sentiment analysis. Must be maximum 512 characters.",
-        example="I love this product! It works great and exceeded my expectations.",
     )
 
 class PredictResponseBody(BaseModel):
     model_used: str = Field(
         description="The name of the model used for prediction, either the base model or the fine-tuned model.",
-        example="distilbert/base-uncased-finetuned-sst-2-english",
     )
     label: str = Field(
         description="The predicted sentiment label, either 'positive', 'neutral' or 'negative'.",
-        example="positive",
     )
     score: float = Field(
         description="The confidence score of the prediction, ranging from 0 to 1.",
-        example=0.95,
     )   
