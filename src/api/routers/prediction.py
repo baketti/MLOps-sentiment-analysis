767e18329a0774f186d3e6119b993efc3f7e3102
baketti <e.bachetti1997@gmail.com>
2026-03-20 22:13:53 +0100

    Refactor some code to use prediction service

--- src/api/routers/prediction.py
+++ src/api/routers/prediction.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, HTTPException, Request
-from predicting.make_prediction import make_prediction
-from utils.config import load_config
 from api.schemas.prediction import PredictRequestBody, PredictResponseBody
-from api.utils.utilities import resolve_model
+from api.services.prediction import make_prediction
 
 router = APIRouter(
     prefix="/predict", 
@@ -13,17 +11,12 @@ router = APIRouter(
 async def predict(payload: PredictRequestBody, request: Request) -> PredictResponseBody:
     try:
         app_config = request.app.state.config
-
-        base_model_name = app_config["hf_model"]["name"]
-        finetuned_model_name = app_config["hf_hub_model_id"]
-
-        model_name = resolve_model(base_model_name, finetuned_model_name)
-        result = make_prediction(payload.text, model_name)
+        result = make_prediction(app_config, payload.text)
+        
+        return PredictResponseBody(
+            model_used=app_config["hf_model"]["name"],
+            label=result["label"],
+            score=result["score"],
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-    return PredictResponseBody(
-        model_used=model_name,
-        label=result["label"],
-        score=result["score"],
-    )
