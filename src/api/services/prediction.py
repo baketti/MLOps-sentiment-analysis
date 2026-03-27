fc1e4e99b2f0233d61755e81bb6129a84cfa8e3c
baketti <e.bachetti1997@gmail.com>
2026-03-27 14:43:10 +0100

    Remove unuseful exception handling

--- src/api/services/prediction.py
+++ src/api/services/prediction.py
@@ -1,6 +1,5 @@
 from predicting.make_prediction import create_sentiment_pipeline, predict
 from api.utils.utilities import resolve_model
-from utils.exceptions import PredictionError
 
 
 def make_prediction(app_config, text: str) -> dict:
@@ -18,16 +17,11 @@ def make_prediction(app_config, text: str) -> dict:
         Raises:
             PredictionError: If there is an error during the prediction
     """
-    try:
-        base_model_name = app_config["hf_model"]["name"]
-        finetuned_model_name = app_config["hf_hub_model_id"]
+    base_model_name = app_config["hf_model"]["name"]
+    finetuned_model_name = app_config["hf_hub_model_id"]
 
-        model_name = resolve_model(base_model_name, finetuned_model_name)
-        sentiment_pipeline = create_sentiment_pipeline(model_name)
-        prediction = predict(text, sentiment_pipeline)
+    model_name = resolve_model(base_model_name, finetuned_model_name)
+    sentiment_pipeline = create_sentiment_pipeline(model_name)
+    prediction = predict(text, sentiment_pipeline)
 
-        return prediction, model_name
-    except PredictionError as e:
-        raise e
-    except Exception as e:
-        raise e
+    return prediction, model_name
